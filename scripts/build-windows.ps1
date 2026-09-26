3b9241835a8d62e2d7c0edd43a3d47580bcd3768
raffmont <raffaele.montella@uniparthenope.it>
2026-09-26 19:54:30 +0200

    Fail Windows builds on native command errors

--- scripts/build-windows.ps1
+++ scripts/build-windows.ps1
@@ -1,4 +1,5 @@
 $ErrorActionPreference = "Stop"
+$PSNativeCommandUseErrorActionPreference = $true
 cmake -S . -B build-windows -G Ninja -DPRG32QT_BUILD_APP=ON @args
 cmake --build build-windows
 ctest --test-dir build-windows --output-on-failure
